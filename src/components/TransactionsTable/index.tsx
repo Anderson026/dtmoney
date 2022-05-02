35c72407c0d39eae9b007cc142ebea80dc8e7c4e
Anderson026 <anderson-ferreira22@hotmail.com>
2022-05-01 22:16:46 -0300

    Criando o componente de transações

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,45 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return(
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Preço</th>
+            <th>Categoria</th> 
+            <th>Data</th>
+          </tr>
+          
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              Desenvolvimento de site
+            </td>
+            <td className="deposit">R$1.200,00</td>
+            <td>Freela</td>
+            <td>01/05/2022</td>
+          </tr>
+          <tr>
+            <td>
+              Desenvolvimento de site
+            </td>
+            <td className="deposit">R$1.200,00</td>
+            <td>Freela</td>
+            <td>01/05/2022</td>
+          </tr>
+          <tr>
+            <td>
+              Aluguel
+            </td>
+            <td className="withdraw">- R$400,00</td>
+            <td>Pagamentos</td>
+            <td>30/04/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
